478c4a33222c99a3a14e9757af333a6d24461cf2
AARON VALERIN PAEZ <aavalerin@estudiantec.cr>
2026-02-26 16:24:09 -0600

    Paso 6 Tarea - Archivo sencillo que contiene 3 errores intencionales detectables por flake8

---
+++ Tarea 1/erroresdelFlaaaaaaaake8.py
@@ -0,0 +1,8 @@
+#Microcontroladores y Microprocesadores - Tarea 1
+# Sebastián Campos y Aarón Valerín
+
+print("Programa para hacer uso de flake 8")
+print("-------------------------------------------------------------------------")
+var= str(input("Digite su nombre: "))
+
+print("Hola", var, ",estamos aprendiendo a usar GitHub en un braunch :D!")
